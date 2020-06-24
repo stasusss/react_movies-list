fdb1842ea16cee2821f9d40eb7deb1f6894ad19b
Stanislav <stasusss07@gmail.com>
2020-06-24 14:35:37 +0300

    Updates

---
+++ precache-manifest.cd8a48d9584e6dfe685b13b947807ed2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92ac02dd9881e873565f0ffb06797a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d210dc0114b5989bd6e",
+    "url": "./static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "140fc47490029e48050b",
+    "url": "./static/css/main.5b5eb71e.chunk.css"
+  },
+  {
+    "revision": "4d210dc0114b5989bd6e",
+    "url": "./static/js/2.23771640.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.23771640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "140fc47490029e48050b",
+    "url": "./static/js/main.1cf0fc82.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
